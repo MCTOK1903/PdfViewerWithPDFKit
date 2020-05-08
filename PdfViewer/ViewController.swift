ce3ee8b69d1103cdae36df73ceccf3b86db3a3e5
MCT <celaltok2@gmail.com>
2020-05-08 22:55:27 +0300

    project init

---
+++ PdfViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PdfViewer
+//
+//  Created by MCT on 8.05.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
